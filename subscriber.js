1d508d01e2ca4b65639af6c2fa43bfa091a176a4
Siddhartha Jain <jainsiddhartha402@gmail.com>
2024-03-07 20:13:16 +0530

    added warning in case of network outage

--- subscriber.js
+++ subscriber.js
@@ -15,18 +15,30 @@ AWS.config.update({
 // Create Amazon Kinesis Video Streams client
 const kinesisVideo = new AWS.KinesisVideo({
     region: aws_region
-  });
+});
   
-  // Parameters to describe the stream
-  const params = {
-    StreamName: aws_stream_name, 
-    APIName: 'GET_HLS_STREAMING_SESSION_URL'
-  };
+// Parameters to describe the stream
+const params = {
+StreamName: aws_stream_name, 
+APIName: 'GET_HLS_STREAMING_SESSION_URL'
+};
+
+
+// Function to display an error message on the webpage
+function displayErrorMessage(message) {
+    var errorMessageDiv = document.getElementById('errorMessage');
+    errorMessageDiv.innerText = message;
+    errorMessageDiv.style.display = 'block'; // Show the error message
+}
+
   
-  // Function to fetch and set the HLS Streaming URL
+// Function to fetch and set the HLS Streaming URL
 function fetchHLSStreamingSessionURL() {
     kinesisVideo.getDataEndpoint(params, function(err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
+        if (err) {
+            console.log(err, err.stack); // an error occurred
+            displayErrorMessage("Network Error: Unable to fetch stream. Please check your connection and try again.");
+        }
         else {
             const endpoint = data.DataEndpoint;
             // Use the endpoint to create a new KinesisVideoArchivedMedia client
@@ -43,7 +55,10 @@ function fetchHLSStreamingSessionURL() {
 
             // Get HLS Streaming Session URL
             kinesisVideoArchivedMedia.getHLSStreamingSessionURL(hlsParams, function(err, data) {
-                if (err) console.log(err, err.stack); // an error occurred
+                if (err){
+                    displayErrorMessage("Network Error: Unable to fetch stream. Please check your connection and try again.");
+                    console.log(err, err.stack); // an error occurred
+                }
                 else {
                     // Now that we have the HLS URL, set it as the video source
                     initializeVideoPlayer(data.HLSStreamingSessionURL);
